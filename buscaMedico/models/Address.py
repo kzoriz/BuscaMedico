05d84b3d582eeed866d0bd3acee0d763c48dc664
kzoriz <borisoliveira@alu.uern.br>
2022-09-06 19:54:28 -0300

    cap 3 finalizado!

--- buscaMedico/models/Address.py
+++ buscaMedico/models/Address.py
@@ -5,7 +5,7 @@ from buscaMedico.models import *
 class Address(models.Model):
     neighborhood = models.ForeignKey(Neighborhood, null=True, related_name='neighborhood', on_delete=models.SET_NULL)
     name = models.CharField(null=False, max_length=100)
-    address = models.BooleanField(null=False, max_length=255, verbose_name='endereço')
+    address = models.CharField(null=False, max_length=255, verbose_name='endereço')
     latitude = models.DecimalField(max_digits=9, decimal_places=7)
     longititude = models.DecimalField(max_digits=9, decimal_places=7)
     opening_time = models.TimeField()
