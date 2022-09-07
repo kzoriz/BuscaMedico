c1d749d60375eb9fd80c6709b46b713be1ea1b51
kzoriz <borisoliveira@alu.uern.br>
2022-09-06 23:51:25 -0300

    conf admin.py

--- buscaMedico/admin.py
+++ buscaMedico/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import *
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    fields = ('user', ('role', ), 'image', 'birthday', 'specialties', 'address')
+    exclude = ('favorites', 'creates_at', 'update_at', )
+    date_hierarchy = 'created_at'
+    list_display = ('user', 'role', 'birthday',)
+    list_display_links = ('user', 'role',)
+    list_filter = ('user__is_active', 'role')
+    empty_value_display = 'Vazio'
+    readonly_fields = ('user',)
+    search_fields = ('user__username', )
+
+
+
 # Register your models here.
-admin.site.register(Profile)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(State)
 admin.site.register(City)
 admin.site.register(Neighborhood)
